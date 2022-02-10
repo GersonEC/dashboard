4b3eb92305e891d0588342d452b2446c65998ff5
GersonEC <gersonenriquez94@gmail.com>
2022-02-10 13:52:13 +0100

    Implement Dashboard Template.

--- src/__tests__/dashboard.test.tsx
+++ src/__tests__/dashboard.test.tsx
@@ -1,11 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import { Dashboard } from '../pages/Dashboard';
 
-describe("Dashboard", () => {
-  it("should contain dashboard text", () => {
-    render(<Dashboard />)
-    
+describe('Dashboard', () => {
+  test('should contain dashboard text', () => {
+    render(<Dashboard />);
     expect(screen.getByText(/dashboard/i)).toBeInTheDocument();
-  
-  })
-})
+  });
+
+  test('should contain the name of the user', () => {
+    render(<Dashboard />);
+    expect(
+      screen.getByText(/Gerson Enriquez/i, { exact: true })
+    ).toBeInTheDocument();
+  });
+
+  test('should contain the role of the user', () => {
+    render(<Dashboard />);
+    expect(screen.getByText(/Admin/i, { exact: true })).toBeInTheDocument();
+  });
+});
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,83 @@
+import styled from 'styled-components';
 
 const Dashboard = () => {
-  return <h1> Dashboard </h1>
-}
+  return (
+    <Wrapper>
+      <Header>
+        <Title>Dashboard</Title>
+        <User>
+          <Name>Gerson Enriquez</Name>
+          <Role>Admin</Role>
+        </User>
+      </Header>
+      <Main>
+        <Nav>
+          <Item>Item</Item>
+          <Item>Caneas</Item>
+          <Item>Gattoir</Item>
+          <Item>Trhtsasfasf</Item>
+        </Nav>
+        <Body></Body>
+      </Main>
+    </Wrapper>
+  );
+};
 
-export {
-  Dashboard
-}
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+  padding: 0 40px;
+`;
+const Title = styled.h2``;
+const User = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  color: gray;
+`;
+const Name = styled.h5`
+  margin: 0;
+`;
+const Role = styled.p`
+  font-size: 0.8rem;
+  margin: 0;
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex: 1;
+`;
+const Nav = styled.nav`
+  width: 150px;
+  margin: 16px 0 16px 6px;
+  display: flex;
+  flex-direction: column;
+`;
+const Item = styled.p`
+  margin: 8px 0;
+  font-weight: 600;
+  color: gray;
+  padding: 8px 0 8px 16px;
+  font-size: 0.9rem;
+  border-radius: 8px;
+  opacity: 0.7;
+  &:hover {
+    cursor: pointer;
+    background: rgba(255, 239, 16, 0.3);
+  }
+`;
+const Body = styled.div`
+  flex: 1;
+  margin: 16px 32px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  border-radius: 8px;
+`;
+export { Dashboard };

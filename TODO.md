a787ac2c07b0f2ed05fe60104783813d80353bec
GersonEC <gersonenriquez94@gmail.com>
2022-02-11 09:59:27 +0100

    Style Login page.

--- TODO.md
+++ TODO.md
@@ -1,10 +1,11 @@
 -Implementare Routing OK
 -Implement Dashboard with TDD OK
 -Implement products feature OK
+-IMPLEMENTING ROUTING DASHBOARD OK
+-Stilare con Styled components. OK
 
--IMPLEMENTING ROUTING DASHBOARD
+-Implement Login Page
 
 -Responsiveness (Desktop first)
--Stilare con Styled components.
 -Theming
 -Unit testing
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,61 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const Login: React.FC = () => {
   return (
-    <div>
-      <h1>Login Page </h1>
+    <>
       <Link to='/products'>
-        <h3>Go to Dashboard</h3>
+        <span>Go to Dashboard</span>
       </Link>
-    </div>
+      <Wrapper>
+        <FormWrapper>
+          <h3>Sign in</h3>
+          <Label>Email</Label>
+          <Label>Password</Label>
+          <Button>Sign in</Button>
+        </FormWrapper>
+      </Wrapper>
+    </>
   );
 };
+
+const Wrapper = styled.div`
+  background-color: rgba(255, 239, 16, 0.1);
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FormWrapper = styled.div`
+  background: white;
+  width: 450px;
+  height: 300px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  border-radius: 8px;
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  color: #adadad;
+  font-size: 0.8rem;
+  font-weight: 700;
+`;
+
+const Button = styled.button`
+  width: 150px;
+  padding: 8px 16px;
+  background-color: #ffd000;
+  color: #696969;
+  font-size: 1rem;
+  font-weight: 600;
+  border-radius: 8px;
+  border: none;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+    transition: 0.3s;
+  }
+`;
